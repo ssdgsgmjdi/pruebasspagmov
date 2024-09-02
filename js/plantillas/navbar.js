769466e0523d6e985f85c92695748e161b8ed9ca
ssdgsgmjdi <ss.jdi@dgsgm.unam.mx>
2024-09-02 13:04:20 -0600

    Update navbar.js
    nav bar anterior

--- js/plantillas/navbar.js
+++ js/plantillas/navbar.js
@@ -1,65 +1,84 @@
 document.write(`
+    <div class="topnav" id="myTopnav">
+    <div class="topnavbar" id="myTopnavbar">
+    <a href="#index" >Inicio</a>
+    <div class="dropdown">
+        <button class="dropbtn">Servicios 
+            <i class="fa fa-caret-down"></i>
+    <div class="dropdownbar">
+        <button class="dropbutton">Servicios 
+            <i class="fa-bar fa-caret-down-bar"></i>
+        </button>
+        <div class="dropdown-content">
+        <div class="dropdown-content-bar">
+            <a href="#">Bicipuma</a>
+            <a href="#">Pumabus</a>
+            <a href="#">Estacionamientos</a>
 
-<ul>
-  <li><a href="#home">Inicio</a></li>
-  <li class="dropdown">
-    <a href="javascript:void(0)" class="dropbtn">Servicios</a>
-    <div class="dropdown-content">
-      <a href="#">Bicipuma</a>
-      <a href="#">Pumabus</a>
-      <a href="#">Estacionamientos</a>
-      <a href="#">Archivo General</a>
-      <a href="#">Área Coordinadora de Archivo</a>
-      <a href="#">Servicios Generales</a>    
-      <a href="#">Brigada Vial</a>       
+        </div>
     </div>
-  </li>
-    <li class="dropdown">
-    <a href="javascript:void(0)" class="dropbtn">Nosotros</a>
-    <div class="dropdown-content">
-      <a href="#">Misión y Visión</a>
-      <a href="#">Funciones</a>
-      <a href="#">Organigrama</a>
-      <a href="#">Directorio</a>
-      <a href="#">Ubicación</a>
-    </div>
-  </li>
-    <li class="dropdown">
-    <a href="javascript:void(0)" class="dropbtn">Contacto</a>
-    <div class="dropdown-content">
-      <a href="#">Redes Sociales </a>
-      <a href="#">WhatsApp</a>
-    </div>
-  </li>
-    <li class="dropdown">
-    <a href="javascript:void(0)" class="dropbtn">Comisiones</a>
-    <div class="dropdown-content">
-      <a href="#">CInIG</a>
-      <a href="#">CLS</a>
-    </div>
-  </li>
-    <li class="dropdown">
-    <a href="javascript:void(0)" class="dropbtn">Preguntas frecuentes</a>
-    <div class="dropdown-content">
-      <a href="#">Generales</a>
-      <a href="#">Bicipuma</a>
-      <a href="#">Pumabus</a>
-      <a href="#">Estacionamientos</a>
-      <a href="#">Archivo General</a>
-      <a href="#">Área Coordinadora de Archivo</a>      
-      <a href="#">Servicios Generales</a>
-      <a href="#">Brigada Vial</a>         
-    </div>
-  </li>
-</ul>
+    <div class="dropdown">
+        <button class="dropbtn">Nosotros 
+            <i class="fa fa-caret-down"></i>
+    <div class="dropdownbar">
+        <button class="dropbutton">Nosotros 
+            <i class="fa-bar fa-caret-down-bar"></i>
+        </button>
+        <div class="dropdown-content">
+        <div class="dropdown-content-bar">
+            <a href="#">Misión y Visión</a>
+            <a href="#">Funciones</a>
+            <a href="#">Organigrama</a>
 
+    </div> 
+    <div class="dropdown">
+        <button class="dropbtn">Contacto 
+            <i class="fa fa-caret-down"></i>
+    <div class="dropdownbar">
+        <button class="dropbutton">Contacto 
+            <i class="fa-bar fa-caret-down-bar"></i>
+        </button>
+        <div class="dropdown-content">
+        <div class="dropdown-content-bar">
+            <a href="#">Redes Sociales</a>
+            <a href="#">WhatsApp</a>          
+        </div>
+    </div> 
+    <div class="dropdown">
+        <button class="dropbtn">Comisiones 
+            <i class="fa fa-caret-down"></i>
+    <div class="dropdownbar">
+        <button class="dropbutton">Comisiones 
+            <i class="fa-bar fa-caret-down-bar"></i>
+        </button>
+        <div class="dropdown-content">
+        <div class="dropdown-content-bar">
+            <a href="#">CInIG</a>
+            <a href="#">CLS</a>          
+        </div>
+    </div> 
+    <div class="dropdown">
+        <button class="dropbtn">Preguntas frecuentes 
+            <i class="fa fa-caret-down"></i>
+    <div class="dropdownbar">
+        <button class="dropbutton">Preguntas frecuentes 
+            <i class="fa-bar fa-caret-down-bar"></i>
+        </button>
+        <div class="dropdown-content">
+        <div class="dropdown-content-bar">
+            <a href="#">Generales</a>        
+            <a href="#">Bicipuma</a>
+            <a href="#">Pumabus</a>
 
 <script>
 function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
   var x = document.getElementById("myTopnavbar");
   if (x.className === "topnavbar") {
     x.className += " responsive";
   } else {
+    x.className = "topnav";
     x.className = "topnavbar";
   }
 }
